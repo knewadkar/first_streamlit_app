4b0b6bee8eb5312559703cb471d1cc3117f1f14d
knewadkar <139284155+knewadkar@users.noreply.github.com>
2023-07-11 15:26:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,12 @@ import streamlit as st
 
 st.title("Title : Hello StreamLit")
 
-st.header('Breakfast Menu')
-st.text(' Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+# st.header('Breakfast Menu')
+# st.text(' Omega 3 & Blueberry Oatmeal')
+# st.text('Kale, Spinach & Rocket Smoothie')
+# st.text('Hard-Boiled Free-Range Egg')
 
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+# st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
